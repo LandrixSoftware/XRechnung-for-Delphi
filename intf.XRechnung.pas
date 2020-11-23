340bc9bc523a1dfd412da2833fbecbe243724d7d
Sven Harazim <sven.harazim@landrix.de>
2020-11-23 10:23:59 +0100

    depency intf.MSXML2_TLB removed

--- intf.XRechnung.pas
+++ intf.XRechnung.pas
@@ -26,7 +26,7 @@ interface
 uses
   System.SysUtils,System.Classes,System.Types,System.DateUtils,System.Rtti
   ,System.Variants,System.StrUtils,System.Generics.Collections
-  ,Xml.xmldom,Xml.XMLDoc,Xml.XMLIntf,Xml.XMLSchema,intf.MSXML2_TLB
+  ,Xml.xmldom,Xml.XMLDoc,Xml.XMLIntf,Xml.XMLSchema//,intf.MSXML2_TLB
   {$IFDEF USE_OXMLDomVendor},OXmlDOMVendor{$ENDIF}
   ,intf.Invoice
   ;

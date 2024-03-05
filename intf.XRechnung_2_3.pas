b1faafdaf59f37c443de6ad738e1ad8d60a123de
Sven Harazim <sven.harazim@landrix.de>
2024-03-05 11:46:33 +0100

    prevent empty company tag

--- intf.XRechnung_2_3.pas
+++ intf.XRechnung_2_3.pas
@@ -1054,7 +1054,8 @@ begin
     with AddChild('cac:PartyLegalEntity') do
     begin
       AddChild('cbc:RegistrationName').Text := _Invoice.AccountingSupplierParty.RegistrationName;
-      AddChild('cbc:CompanyID').Text := _Invoice.AccountingSupplierParty.CompanyID;
+      if _Invoice.AccountingSupplierParty.CompanyID <> '' then
+        AddChild('cbc:CompanyID').Text := _Invoice.AccountingSupplierParty.CompanyID;
     end;
     with AddChild('cac:Contact') do
     begin
@@ -1103,7 +1104,8 @@ begin
     with AddChild('cac:PartyLegalEntity') do
     begin
       AddChild('cbc:RegistrationName').Text := _Invoice.AccountingCustomerParty.RegistrationName;
-      AddChild('cbc:CompanyID').Text := _Invoice.AccountingCustomerParty.CompanyID;
+      if _Invoice.AccountingCustomerParty.CompanyID <> '' then
+        AddChild('cbc:CompanyID').Text := _Invoice.AccountingCustomerParty.CompanyID;
       //TODO <cbc:CompanyLegalForm>123/456/7890, HRA-Eintrag in []</cbc:CompanyLegalForm>
     end;
     with AddChild('cac:Contact') do
@@ -1436,7 +1438,8 @@ begin
         //<cbc:CompanyLegalForm>123/456/7890, HRA-Eintrag in []</cbc:CompanyLegalForm>
         with AddChild('ram:SpecifiedLegalOrganization') do
         begin
-          AddChild('ram:ID').Text := _Invoice.AccountingSupplierParty.CompanyID;
+          if _Invoice.AccountingSupplierParty.CompanyID <> '' then
+            AddChild('ram:ID').Text := _Invoice.AccountingSupplierParty.CompanyID;
           AddChild('ram:TradingBusinessName').Text := _Invoice.AccountingSupplierParty.Name;
         end;
         with AddChild('ram:DefinedTradeContact') do
@@ -1475,7 +1478,8 @@ begin
 
         with AddChild('ram:SpecifiedLegalOrganization') do
         begin
-          AddChild('ram:ID').Text := _Invoice.AccountingCustomerParty.CompanyID;
+          if _Invoice.AccountingCustomerParty.CompanyID <> '' then
+            AddChild('ram:ID').Text := _Invoice.AccountingCustomerParty.CompanyID;
           AddChild('ram:TradingBusinessName').Text := _Invoice.AccountingCustomerParty.Name;
         end;
         with AddChild('ram:DefinedTradeContact') do
